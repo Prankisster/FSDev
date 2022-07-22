f94c900b61e2addf8f0f12da09f98ba4b55c16e2
Prankisster <92308896+Prankisster@users.noreply.github.com>
2022-07-22 17:35:45 +0530

    Added Files

---
+++ interactiveRatingChallange/Required_Js.js
@@ -0,0 +1,8 @@
+
+function typage(val)
+{
+    mes="You selected " +val+ " out of 5";
+    document.getElementById('cm').innerHTML=mes;
+    document.getElementById('first').style.display="none";
+    document.getElementById('second').style.display="block";
+}
